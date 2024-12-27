cf752985f773dd712841701f49247aa56a21afbd
lihan1001 <victoriam713jk@gmail.com>
2024-12-28 00:40:35 +0800

    .

--- recipe/recipe_crawler.py
+++ recipe/recipe_crawler.py
@@ -15,6 +15,11 @@ CORS(app)
 #app的路由地址"/fetch_recipes"即為ajax中定義的url地址，采用POST提交
 @app.route('/fetch_recipes',methods=["POST"])
 
+@app.route('/')
+def index():
+    return "Welcome to the Recipe Fetcher!"
+
+
 #從這里定義具體的函式 回傳值均為json格式
 def fetch_recipes():
     base_url = "https://icook.tw/recipes/search?q={}&page={}"
