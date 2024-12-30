a886b51b0b36477910d5ba3c93fb7c7c8901b425
lihan1001 <victoriam713jk@gmail.com>
2024-12-31 00:45:28 +0800

    p

--- src/quickstart.py
+++ src/quickstart.py
@@ -33,7 +33,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("../dict/credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
